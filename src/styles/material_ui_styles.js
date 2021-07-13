3cca98a69d7a27c30b085c73e1f51a4a511f2e17
Varad Gauthankar <gvarad1601@gmail.com>
2021-07-13 17:56:22 +0530

    fix text alignment issue

--- src/styles/material_ui_styles.js
+++ src/styles/material_ui_styles.js
@@ -2,7 +2,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
   formControl: {
-    // margin: theme.spacing(1),
+    // margin: theme.spacing(1)
+    textAlign: "left",
     minWidth: 120,
     maxWidth: 220,
   },
